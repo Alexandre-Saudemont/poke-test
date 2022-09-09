553f45419351dafc977aa692634d2ee993d61321
Adam <adam.gaoua@gmail.com>
2022-09-09 17:13:49 +0200

    adding redirection & timeout to inscriptionjs

--- client/src/components/Inscription/Inscription.js
+++ client/src/components/Inscription/Inscription.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
 import { RegisterRequest } from '../../requests'
+import {useNavigate} from "react-router-dom"
 import Input from '@mui/material/Input';
 import InputLabel from '@mui/material/InputLabel';
 import Button from '@mui/material/Button';
 import { FormHelperText } from '@mui/material';
+import  Modal  from '@mui/material/Modal';
 import './Inscription.css';
 
 function Inscription({setIsActive}) {
@@ -15,7 +17,35 @@ function Inscription({setIsActive}) {
     const [lastname, setLastname] = useState("");
     const [success, setSuccess] = useState("");
     const [error, setError] = useState("");
+    const [open, setOpen] = useState(false);
+    const [number, setNumber] = useState(5)
+    const navigate = useNavigate();
+    const style = {
+        position: 'relative',
+        top: '30%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width:"13rem",
+        heigth:"10rem",
+        bgcolor: 'rgba(54, 89, 89, 0.65)',
+        textAlign: 'center',
+        border: '2px solid #000',
+        color: "#C7C7C7",
+        boxShadow: 24,
+        p: 4,
+        mt: 10,
+        borderRadius:'15px',
+        fontWeigth:'bold',
+      };
 
+    const timeOutFunction = () => {
+        navigate('/Connexion');
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+    
     async function handleSubmit(e) {
 
         e.preventDefault()
@@ -26,14 +56,13 @@ function Inscription({setIsActive}) {
             }
 
             if (response.status === 201) {
-                setSuccess(response.data.success);
-                
-                setIsActive(false)
-                setEmail("");
-                setFirstname("");
-                setLastname("");
-                setUsername("");
-                setPassword("");
+                setSuccess(response.data.success);                
+                setIsActive(false);
+                setTimeout(timeOutFunction, 5000)    
+                setOpen(true); 
+                setInterval(() => {
+                    setNumber(prev=>prev-1); 
+                }, 1000)       
             }
 
         } catch (error) {
@@ -46,7 +75,19 @@ function Inscription({setIsActive}) {
     return (
         <div className="inscription-container">
             {success ?
-                <p className="success">{success}</p> :
+                <Modal 
+                    open={open} 
+                    onClose={handleClose} 
+                    className="inscription-modal"
+                    sx={style}
+                >
+                    <div>
+                         {success} 
+                         <p>Redirection vers Connexion dans {number}...</p>
+                    </div>
+                    
+                         
+                </Modal> :
                 <>
                     <div>
                         <h2 className="inscription-title">Inscription</h2>
